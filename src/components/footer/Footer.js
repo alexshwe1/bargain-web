afe816bfbe4e7ececa7016237bda04e488f7be17
alexshwe1 <alexander.shwe@gmail.com>
2023-09-05 12:37:37 -0500

    trying hashrouter for github pages

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,12 +1,18 @@
+// Footer.js
 import React from 'react';
-import './Footer.css'; // Import the footer.css file from the same directory
+import './Footer.css'
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
     <footer className="footer">
       <ul>
-        <li><a href="/privacy-policy">Privacy Policy</a></li>
-        <li><a href="/support">Support</a></li>
+        <li>
+          <Link to="/privacy-policy">Privacy Policy</Link>
+        </li>
+        <li>
+          <Link to="/support">Support</Link>
+        </li>
       </ul>
     </footer>
   );
