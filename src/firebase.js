69424ad6fc6c392b2653ab08232d9a5b5a30909f
alexshwe1 <alexander.shwe@gmail.com>
2023-10-13 11:16:13 -0500

    added firebase package

---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDglTVewUX4QoIlTZ8wxl5jl9SR7Bhv5S8",
+  authDomain: "bargain-web.firebaseapp.com",
+  projectId: "bargain-web",
+  storageBucket: "bargain-web.appspot.com",
+  messagingSenderId: "58984318047",
+  appId: "1:58984318047:web:605fcc0006bf8c1ad787ea",
+  measurementId: "G-6J671LYP2C"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
